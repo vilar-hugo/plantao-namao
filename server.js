15452b227aa772744f1315ecb1fb77caca6a1207
vilar-hugo <vilar.hugo@gmail.com>
2026-04-05 17:12:14 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  fs.createReadStream(
+    path.join(__dirname, 'plantaocerto_admineasy.html')
+  ).pipe(res);
+}).listen(PORT, () => {
+  console.log('Plantao Certo rodando na porta ' + PORT);
+});
